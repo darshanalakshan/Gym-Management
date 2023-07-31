0aca046166865fd6ad27d69d935ebda9f792ff33
darshanalakshan <darshanalakshan997@gmail.com>
2023-07-31 11:01:57 +0530

    New Memeber Dashboard UI Updated.

--- Gym Management/Forms/New Member.Designer.cs
+++ Gym Management/Forms/New Member.Designer.cs
@@ -28,12 +28,296 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "New_Member";
+            label1 = new Label();
+            panel1 = new Panel();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label5 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label6 = new Label();
+            dateTimePicker2 = new DateTimePicker();
+            label7 = new Label();
+            textBox3 = new TextBox();
+            label8 = new Label();
+            comboBox1 = new ComboBox();
+            label9 = new Label();
+            comboBox2 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Nirmala UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(3, 15);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(257, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Add New Member";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1155, 71);
+            panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 80);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(115, 28);
+            label2.TabIndex = 2;
+            label2.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Location = new Point(11, 122);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(300, 34);
+            textBox1.TabIndex = 3;
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(395, 80);
+            label3.Name = "label3";
+            label3.Size = new Size(112, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Last Name";
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Location = new Point(395, 122);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(300, 34);
+            textBox2.TabIndex = 5;
+            textBox2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(11, 182);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 28);
+            label4.TabIndex = 6;
+            label4.Text = "Gender";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(46, 224);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(80, 32);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(160, 224);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(100, 32);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(395, 182);
+            label5.Name = "label5";
+            label5.Size = new Size(102, 28);
+            label5.TabIndex = 9;
+            label5.Text = "Join Date";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(399, 227);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(300, 34);
+            dateTimePicker1.TabIndex = 10;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(399, 299);
+            label6.Name = "label6";
+            label6.Size = new Size(137, 28);
+            label6.TabIndex = 11;
+            label6.Text = "Date of Birth";
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Format = DateTimePickerFormat.Short;
+            dateTimePicker2.Location = new Point(399, 349);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(300, 34);
+            dateTimePicker2.TabIndex = 12;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 299);
+            label7.Name = "label7";
+            label7.Size = new Size(161, 28);
+            label7.TabIndex = 13;
+            label7.Text = "Mobile Number";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(11, 349);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(300, 34);
+            textBox3.TabIndex = 14;
+            textBox3.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 412);
+            label8.Name = "label8";
+            label8.Size = new Size(140, 28);
+            label8.TabIndex = 15;
+            label8.Text = "Package Type";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DisplayMember = "Select Package Type";
+            comboBox1.FlatStyle = FlatStyle.System;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Workout", "Workout + Treadmill" });
+            comboBox1.Location = new Point(12, 467);
+            comboBox1.Name = "comboBox1";
+            comboBox1.RightToLeft = RightToLeft.No;
+            comboBox1.Size = new Size(300, 36);
+            comboBox1.TabIndex = 16;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(399, 412);
+            label9.Name = "label9";
+            label9.Size = new Size(183, 28);
+            label9.TabIndex = 17;
+            label9.Text = "Membership Time";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FlatStyle = FlatStyle.System;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Monthly", "6 Months", "12 Months" });
+            comboBox2.Location = new Point(399, 467);
+            comboBox2.Name = "comboBox2";
+            comboBox2.RightToLeft = RightToLeft.No;
+            comboBox2.Size = new Size(300, 36);
+            comboBox2.TabIndex = 18;
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderColor = Color.Black;
+            button1.FlatAppearance.MouseDownBackColor = Color.Gainsboro;
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 73, 172);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(194, 560);
+            button1.Name = "button1";
+            button1.Size = new Size(282, 69);
+            button1.TabIndex = 19;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.MouseDownBackColor = Color.Silver;
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(75, 73, 172);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(509, 560);
+            button2.Name = "button2";
+            button2.Size = new Size(282, 69);
+            button2.TabIndex = 20;
+            button2.Text = "Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // New_Member
+            // 
+            AutoScaleDimensions = new SizeF(12F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(245, 247, 255);
+            ClientSize = new Size(1155, 657);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox2);
+            Controls.Add(label9);
+            Controls.Add(comboBox1);
+            Controls.Add(label8);
+            Controls.Add(textBox3);
+            Controls.Add(label7);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(label6);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label5);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Font = new Font("Nirmala UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(5);
+            Name = "New_Member";
+            Text = "New_Member";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private Label label6;
+        private DateTimePicker dateTimePicker2;
+        private Label label7;
+        private TextBox textBox3;
+        private Label label8;
+        private ComboBox comboBox1;
+        private Label label9;
+        private ComboBox comboBox2;
+        private Button button1;
+        private Button button2;
     }
 }
